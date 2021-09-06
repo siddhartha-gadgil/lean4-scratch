c18c8672ae42b105d56b5ee13ec32700ab5eb956
Siddhartha Gadgil <siddhartha.gadgil@gmail.com>
2021-09-06 16:49:40 +0530

    more experiments

--- Scratch/Egs.lean
+++ Scratch/Egs.lean
@@ -36,8 +36,8 @@ fun stx expectedType? =>
       let s0 ← s
       let expr0 ←  elabTerm s0 (some (Lean.mkConst `Nat)) 
       let name2 : Name := `Nat.succ
-      let expr : Expr :=  
-        mkApp (Lean.mkConst name2)  expr0 
+      let expr : Expr ←  
+        mkAppM name2  #[expr0] 
             -- the expression returned by a function
       return expr
   | _ =>
@@ -86,23 +86,38 @@ fun stx expectedType? =>
 #eval Nat.succ >>> 3
 #eval Nat.succ >>> true
 
+def eee := Nat.succ >>> 3
+#eval eee
+
+def egLocal := 
+  let fn : Nat → Nat := fun n => n + n 
+  fn >>> 3
+
+#eval egLocal
+
 inductive Someterm  where
-  | something : {α : Type} → (a: α ) → Someterm
+  | something  : {α : Type} → (a: α ) → Someterm
   | nothing : Someterm
+
+def Someterm.isEmpty : Someterm → Bool 
+  | Someterm.something  _ => false
+  | Someterm.nothing => true
 
 syntax (name := tryapp2) term " >>>> " term : term
 
 @[termElab tryapp2] def tryappImpl2 : TermElab :=
   let nt := Lean.mkConst `Someterm.nothing
   let st := Lean.mkConst `Someterm.something
+  
   fun stx expectedType? =>
     match stx with
     | `($s >>>> $t) =>
       do
         let f <- elabTerm s none
         let x <- elabTerm t none
-        let expr : Expr := mkApp f x
-        let c ←  isTypeCorrect expr 
+        let expr : Expr ←  mkApp f x
+        let c ←  isTypeCorrect expr
+        -- let cc := !expr.hasExprMVar
         if c then
           return Lean.mkApp st expr
         else
@@ -113,3 +128,90 @@ syntax (name := tryapp2) term " >>>> " term : term
 
 #check Nat.succ >>>> 3
 #check Nat.succ >>>> true
+
+
+#check (Eq.trans >>>> (rfl : Nat.zero = Nat.zero)) 
+        
+
+def optApp {α β γ : Type} (f : α → β) (x : γ)  :=
+  f >>>> x
+
+#print optApp
+
+def eg3 := optApp Nat.succ 3
+
+def eg4 := optApp Nat.succ true
+
+#eval eg3.isEmpty -- this fails, the definition does not type check
+#eval eg4.isEmpty
+
+#print eg3
+
+def exprApp (e1 e1t e2 : Expr) : MetaM Expr :=
+  let n := Name.mkSimple "unsafe-name"
+  withLetDecl n e1t e1 fun x => do
+    let b ←  (mkAppM n #[e2])
+    return ← (mkLetFVars #[x] b)
+
+syntax (name := unapp) term " :: " term " |< " term : term
+
+@[termElab unapp] def unappImpl : TermElab :=
+  let nt := Lean.mkConst `Someterm.nothing
+  let st := Lean.mkConst `Someterm.something  
+  let n := Name.mkSimple "unsafe-name"
+  fun stx expectedType? =>
+    match stx with
+    | `($s :: $t |< $u) =>
+      do
+        let f <- elabTerm s none
+        let type ← elabTerm t none
+        let z <- elabTerm u none
+        let expr : Expr ←  withLetDecl n type f fun x => do
+                              let b ←  (mkAppM n #[z])
+                              return ← (mkLetFVars #[x] b)
+        let c <- isTypeCorrect expr
+        if c then
+          return Lean.mkApp st expr
+        else
+          return nt
+    | _ => 
+      do 
+        return nt
+
+-- #check Nat.succ :: (Nat → Nat) |< 3
+
+set_option pp.raw true
+set_option pp.raw.maxDepth 10
+
+#print unappImpl
+#print exprApp
+
+syntax (name := minlet) "minlet!" : term
+
+@[termElab minlet] def minletImpl : TermElab :=
+  fun stx expectedType? =>
+  let n := Name.mkSimple "n"
+  let z := Lean.mkConst `Nat.zero
+  let ty := Lean.mkConst `Nat
+  withLetDecl n ty z fun x => do
+    let e <- mkLetFVars #[x] x
+    return e
+
+#print minletImpl  
+#check minlet!
+
+def nameLess (name: Name) := 1
+
+syntax (name := ignorename) "ignore!" ident : term
+
+macro_rules
+  | `(ignore! $s) => `(nameLess `$s)
+
+#check nameLess ``Nat.succ
+
+inductive WrapTerm where
+  | wrap : {α : Type} → (a: α ) → WrapTerm
+  | wrapName : Name → WrapTerm
+  | wrapExpr : Array Expr → WrapTerm
+
+#check WrapTerm
