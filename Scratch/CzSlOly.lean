6a528005cfdebc9d269332c3ae10bd43b0b94e66
Siddhartha Gadgil <siddhartha.gadgil@gmail.com>
2021-10-19 17:39:44 +0530

    clean up of proof

--- Scratch/CzSlOly.lean
+++ Scratch/CzSlOly.lean
@@ -7,14 +7,14 @@ theorem CzSlOly : (∀ a b : M, (a * b) * b = a) → (∀ a b : M, a * (a * b) =
               intros ax1 ax2 m n
               have lem1 : (m * n) * n = m := ax1 m n
               have lem2 : (m * n) * ((m * n) * n) = n := ax2 (m * n) n
+              have lem3 : ((m * n) * m) * m = m * n  := ax1 (m * n) m
               have lem4 : (m * n) * ((m * n) * n) = (m * n) * m := 
-                  congrArg (fun x => (m * n) * x) lem1
-              have lem5 : ((m * n) * m) * m = m * n:= ax1 (m * n) m
-              have lem6 : (m * n) * m = n := by
+                  congrArg (fun x => (m * n) * x) lem1              
+              have lem5 : (m * n) * m = n := by
                     rw [lem4] at lem2
                     assumption
-              have lem7 : ((m * n) * m) * m = n * m  := 
-                    congrArg (fun x => x * m) lem6 
-              rw [lem5] at lem7
+              have lem6 : ((m * n) * m) * m = n * m  := 
+                    congrArg (fun x => x * m) lem5 
+              rw [lem3] at lem6
               assumption 
               
