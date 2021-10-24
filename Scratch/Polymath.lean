27cba595904c9457737823d98b7254d3eb42644b
Siddhartha Gadgil <siddhartha.gadgil@gmail.com>
2021-10-24 18:05:32 +0530

    some cleanup

--- Scratch/Polymath.lean
+++ Scratch/Polymath.lean
@@ -73,6 +73,34 @@ def ProvedSplit.prepend{l: Letter}{w : Word} (x: Letter)
           simp
       ⟨newFst, newSnd, newProof⟩   
 
+def provedSplits(z: Letter) : (w : Word) → List (ProvedSplit z w) 
+  | [] => []
+  | x :: ys =>
+    let tailSplits := (provedSplits z ys).map (ProvedSplit.prepend x) 
+    if c:x = z then
+      let headSplit : ProvedSplit z (x :: ys) :=
+        by
+          rw [c] 
+          exact ProvedSplit.head z ys 
+      headSplit :: tailSplits
+    else tailSplits
+
+#eval (provedSplits α [β, α, β, α, β⁻¹]).map (fun ps => (ps.fst, ps.snd))
+
+abbrev Length := Word → Nat
+
+def conjInv(l: Length) : Prop := (x : Letter) → (g : Word) → l (g^x) = l (g)
+
+def triangIneq(l: Length) : Prop := (g h : Word) → l (g ++ h) ≤ l g + l h
+
+def normalized(l: Length) : Prop := (x : Letter) → l [x] = 1
+
+def emptyWord(l: Length) : Prop := l [] = 0
+
+structure ProvedBound(g: Word):  Type where
+  bound: Nat 
+  pf : (l: Length) → emptyWord l → 
+           normalized l → conjInv l → triangIneq l → l g ≤ bound
 
 theorem conj_split (x: Letter) (ys fst snd: Word) :
           ys = fst ++ [x⁻¹] ++ snd → x :: ys = fst^x ++ snd :=
@@ -80,31 +108,11 @@ theorem conj_split (x: Letter) (ys fst snd: Word) :
               intro hyp
               have expand : fst^x = [x] ++ fst ++ [x⁻¹] := rfl
               rw [expand] 
-              have lemma := 
-                    congrArg (List.cons x) hyp
               rw [hyp]
               simp
 
-abbrev Length := Word → Nat
-
-def conjInv(l: Length) : Prop := 
-    (x : Letter) → (g : Word) → l (g^x) = l (g)
-
-def triangIneq(l: Length) : Prop := 
-    (g h : Word) → l (g ++ h) ≤ l g + l h
-
-def normalized(l: Length) : Prop :=
-    (x : Letter) → l [x] = 1
-
-def emptyWord(l: Length) : Prop :=
-    l [] = 0
-
-structure ProvedBound(g: Word):  Type where
-  bound: Nat 
-  pf : (l: Length) → emptyWord l → 
-           normalized l → conjInv l → triangIneq l → l g ≤ bound
-
-def ProvedBound.conj(x: Letter)(ys fst snd: Word)(eqn : ys = fst ++ [x⁻¹] ++ snd) :
+def ProvedBound.matchHead(x: Letter)(ys fst snd: Word)
+      (eqn : ys = fst ++ [x⁻¹] ++ snd) :
         ProvedBound fst → ProvedBound snd → ProvedBound (x :: ys) := 
           fun pb1 pb2 =>
           let bound := pb1.bound + pb2.bound
@@ -129,39 +137,20 @@ def ProvedBound.prepend{w : Word} (x: Letter)
       let newBound := ps.bound + 1
       ⟨newBound, fun l emp norm conj triang => by
         let prev := ps.pf l emp norm conj triang
-        have lemTri : l (x :: w) ≤ l ([x]) + l w := 
-          by
-            apply triang [x] 
+        have lemTri : l (x :: w) ≤ l ([x]) + l w := by apply triang [x] 
         apply Nat.le_trans lemTri
         rw [norm x]
         rw [Nat.add_comm]
-        apply Nat.add_le_add_right prev  
-        ⟩
+        apply Nat.add_le_add_right prev⟩
+
+def ProvedBound.emptyWord: ProvedBound [] :=
+  ⟨0, fun l emp _ _ _ => by rw [emp]; apply Nat.le_refl⟩
 
 def ProvedBound.min {w: Word}: ProvedBound w → List (ProvedBound w) → 
     ProvedBound w :=
         fun head tail =>
           tail.foldl (fun pb1 pb2 =>
             if pb1.bound ≤ pb2.bound then pb1 else pb2) head
-
-def ProvedBound.emptyWord: ProvedBound [] :=
-  ⟨0, fun l emp _ _ _ => by rw [emp]; apply Nat.le_refl⟩
-
-def provedSplits(z: Letter) : (w : Word) → List (ProvedSplit z w) 
-  | [] => []
-  | x :: ys =>
-    let tailSplits := (provedSplits z ys).map (ProvedSplit.prepend x) 
-    if c:x = z then
-      let headSplit : ProvedSplit z (x :: ys) :=
-        by
-          rw [c] 
-          exact ProvedSplit.head z ys 
-      headSplit :: tailSplits
-    else tailSplits
-
-#eval (provedSplits α [β, α, β, α, β⁻¹]).map (fun ps => (ps.fst, ps.snd))
-
-#check Nat.add_le_add_right
 
 def easyBound : (w : Word) → ProvedBound w 
   | [] => ProvedBound.emptyWord
@@ -181,10 +170,8 @@ def easyBound : (w : Word) → ProvedBound w
           exact base.pf l emp norm conj triang 
     ⟨bound, pf⟩
 
-instance {w: Word} : Inhabited (ProvedBound w) :=
-  ⟨easyBound w⟩
+instance {w: Word} : Inhabited (ProvedBound w) :=⟨easyBound w⟩
   
-
 partial def provedBound : (w: Word) → ProvedBound w := fun w =>
   match w with
   | [] => ProvedBound.emptyWord
@@ -192,7 +179,7 @@ partial def provedBound : (w: Word) → ProvedBound w := fun w =>
     let head := ProvedBound.prepend x (provedBound ys)
     let splits := provedSplits (x⁻¹ )  ys
     let tail := splits.map (fun ps => 
-      ProvedBound.conj x ys ps.fst ps.snd ps.proof (provedBound ps.fst)
+      ProvedBound.matchHead x ys ps.fst ps.snd ps.proof (provedBound ps.fst)
           (provedBound ps.snd))
     ProvedBound.min head tail
     
