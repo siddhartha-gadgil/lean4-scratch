158c51ef949157f3c77c74f85d113d9ae65f5cf2
Siddhartha Gadgil <siddhartha.gadgil@gmail.com>
2021-11-06 21:53:56 +0530

    refactoring to separate generation from tactic

--- Scratch/IntrosRwFind.lean
+++ Scratch/IntrosRwFind.lean
@@ -53,6 +53,31 @@ def addAllToContextM (values : List Expr) :
           let newMVarIds ← addToContextM Name.anonymous (← inferType h) h m
           addAllToContextM t newMVarIds.head!
 
+def generateSeek(n: Nat)(nameOpt: Option Name)(introVars: Array FVarId)
+      (goalNames : List Name)(mvar: MVarId)
+      (dynamics : Nat → Array Expr → Array Name → TermElabM (Array Expr)) : TacticM Unit :=
+              withMVarContext mvar do
+          let introFreeVars := introVars.map (fun x => mkFVar x)
+          -- logInfo m!"intros : {← types introFreeVars.toList}"
+          let target ←  getMVarType mvar
+          let evolved ← dynamics n  (introFreeVars) goalNames.toArray
+          let exported ← evolved.mapM (
+                      fun e => mkLambdaFVars introFreeVars e) 
+          let found ← evolved.findM? (fun e => do isDefEq (← inferType e) target)
+          match found with
+          | some x => 
+            do
+              logInfo m!"found : {x}"
+              logInfo m!"found-type: {← inferType x}"
+              assignExprMVar mvar x
+              replaceMainGoal []
+          | none => 
+            replaceMainGoal [mvar]
+          match nameOpt with
+            | some name => saveExprArr name exported
+            | none => return ()
+          return ()
+
 syntax (name:= introsRwFind) "introsRwFind" (term ("save:" ident)?)?: tactic
 @[tactic introsRwFind] def introsRwfindImpl : Tactic :=
   fun stx  =>
@@ -72,50 +97,10 @@ syntax (name:= introsRwFind) "introsRwFind" (term ("save:" ident)?)?: tactic
       where introsRWAux (n: Nat)(nameOpt: Option Name) : TacticM Unit :=
         withMainContext do
         let mvar ← getMainGoal
+        let goalNames ← ConstDeps.recExprNames (← getEnv) (← getMainTarget)
         let ⟨introVars, codmvar⟩ ← Meta.intros mvar
-        withMVarContext codmvar do
-          let introFreeVars := introVars.map (fun x => mkFVar x)
-          let target ←  getMVarType codmvar
-          logInfo m!"intros : {← types introFreeVars.toList}"
-          let goalPieces ← exprPieces target 
-          let goalNames ← ConstDeps.recExprNames (← getEnv) target
-          logInfo m!"goalNames : {goalNames}"
-          let evolved ← iterAppRWTask n  (introFreeVars) goalNames.toArray
-          -- let unit ←  match nameOpt with
-          --   | some name => saveExprArr name evolved
-          --   | none => return ()
-          -- logInfo m!"generated types : {← evolved.mapM (fun e => inferType e)}"
-          -- logInfo m!"generated terms : {evolved}"
-          let found ← evolved.findM? (fun e => do isDefEq (← inferType e) target)
-      --     let found ← typInList? target evolved
-          match found with
-          | some x => 
-            do
-              logInfo m!"found : {x}"
-              logInfo m!"found-type: {← inferType x}"
-              assignExprMVar codmvar x
-              replaceMainGoal []
-              let unit ←  match nameOpt with
-                | some name =>
-                  let exported ← evolved.mapM (
-                      fun e => mkLambdaFVars introFreeVars e) 
-                  saveExprArr name exported
-                | none => return ()
-              return ()
-          | none => 
-            replaceMainGoal [codmvar]
-            -- let value ← TermSeq.pack evolved.toList
-            -- let type ← inferType value
-            -- let name := `genpack
-            -- liftMetaTactic $  addToContextM name type value 
-            let unit ←  match nameOpt with
-                | some name =>
-                  let exported ← evolved.mapM (
-                      fun e => mkLambdaFVars introFreeVars e) 
-                  saveExprArr name exported
-                | none => return ()
-            return ()
-
+        logInfo m!"goalNames : {goalNames}"
+        generateSeek n nameOpt introVars goalNames codmvar iterAppRWTask
 
 def modusPonens : {α β : Type} →  α → (α → β) → β := by
       introsRwFind 1 save:blah
