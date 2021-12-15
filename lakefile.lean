4e4967ccb3c9c8d24e69395ac01ff3d22e2a7429
Siddhartha Gadgil <siddhartha.gadgil@gmail.com>
2021-12-15 18:53:38 +0530

    Mathlib dependency

--- lakefile.lean
+++ lakefile.lean
@@ -2,6 +2,10 @@ import Lake
 open Lake DSL
 
 package scratch {
+  dependencies := #[{
+    name := "mathlib",
+    src := Source.git "https://github.com/leanprover-community/mathlib4.git" "80905943d92abcd01ec69b6054f6dbc2a718c76d"
+  }],
   supportInterpreter := true
   -- add configuration options here
 }
