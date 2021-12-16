ac09c148c66daaa5ef4f800a9d42a601e81ebf50
Siddhartha Gadgil <siddhartha.gadgil@gmail.com>
2021-12-16 14:25:36 +0530

    mathlib triples

--- Scratch/MathDeps.lean
+++ Scratch/MathDeps.lean
@@ -29,3 +29,19 @@ def writeBlob : IO Unit := do
   return ()
 
 #eval writeBlob
+
+def mathTriplesString : IO String := do
+  let pairs ← offSpringTriple mathEnv none none
+  let blob : String := pairs.foldl (
+        fun s (p, l, lt) => 
+          s ++ "[" ++  (p.toString) ++ "," ++ l.toString  
+            ++ "," ++ lt.toString ++ "]\n") ""
+  return blob
+
+def writeTriples : IO Unit := do
+  let blob ← mathTriplesString
+  let file := System.mkFilePath ["data/mathTriples.txt"]
+  IO.FS.writeFile file blob
+  return ()
+
+#eval writeTriples
